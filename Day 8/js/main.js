71542b05bf5fb3ceb022ba3356d1d1b42292e817
Yesid20 <edgarcarloscamilo@gmail.com>
2024-04-25 08:22:35 -0500

    feat: :sparkles: Se consumio una API con elementos llave - valor y elementos con links.
    Se evidencio el mostrar el informacion de todas las llaves - valor y sus respectivos datos dentro de llave que alojaban link o arrays de links

--- Day 8/js/main.js
+++ Day 8/js/main.js
@@ -16,11 +16,9 @@ function fetchStarWars() {
                         <p class="card-text">Gender: ${data.gender}</p>
                         <p class="card-text">Homeworld:</p>
                         <div id="homeworldInfo"></div> <!-- Aquí se mostrará la información del planeta -->
-                        <ul class="list-group">
-                            ${data['films'].map(alias => `<li class="list-group-item">${alias}</li>`).join('')}
-                        </ul>
-                        <p class="card-text">Species: ${data.species}</p>
-                        <p class="card-text">Vehicles: ${data.vehicles}</p>
+                        <div id="filmsInfo"></div> <!-- Aquì se mostrarà la informaciòn del planeta -->
+                        <div id="speciesInfo"></div> <!-- Aquì se mostrara la informaciòn del planeta -->
+                        <div id="vehiclesInfo"></div> <!-- Aquì se mostrara la informaciòn del planeta -->
                         <p class="card-text">Starships: ${data.starships}</p>
                         <p class="card-text">Created: ${data.created}</p>
                         <p class="card-text">Edited: ${data.edited}</p>
@@ -52,8 +50,73 @@ function fetchStarWars() {
                 .catch(error => {
                     console.error('Error al obtener datos del planeta:', error);
                 });
+            // Realiza una solicitud a las movies 
+                fetch(data.films[0])
+                .then(response => response.json())
+                .then(filmData => {
+                    const filmsInfo = document.getElementById('filmsInfo');
+                    // Construye la informaciòn de las movies
+                    const filmInfo = `
+                        <p>Title:${filmData.title}</p>
+                        <p>Episode:${filmData.episode_id}</p>
+                        <p>Director:${filmData.director}</p>
+                        <p>Producer:${filmData.producer}</p>
+                        <p>Release Date:${filmData.release_date}</p>
+                    `;
+                    // Agrega la informacion del planeta al div movies
+                    console.log(filmInfo);
+                    filmsInfo.innerHTML = filmInfo;
+                })
+                .catch(error => {
+                    console.error('Error fetching  film:', error);
+                });
+
+            // Realiza una solicitud a las movies 
+            fetch(data.species[0])
+            .then(response => response.json())
+            .then(specieData => {
+                const speciesInfo = document.getElementById('speciesInfo');
+                // Construye la informaciòn de las movies
+                const specieInfo = `
+                    <p>Name:${specieData.name}</p>
+                    <p>Episode:${specieData.classification}</p>
+                    <p>Director:${specieData.designation}</p>
+                    <p>Producer:${specieData.average_height}</p>
+                    <p>Release Date:${specieData.skin_colors}</p>
+                `;
+                // Agrega la informacion del planeta al div movies
+                console.log(specieInfo);
+                speciesInfo.innerHTML = specieInfo;
+            })
+            .catch(error => {
+                console.error('Error fetching  specie:', error);
+            });
+
+            
+            // Realiza una solicitud a las movies 
+            fetch(data.vehicles[0])
+            .then(response => response.json())
+            .then(vehicleData => {
+                const vehiclesInfo = document.getElementById('vehiclesInfo');
+                // Construye la informaciòn de las movies
+                const vehicleInfo = `
+                    <p>Name:${vehicleData.name}</p>
+                    <p>Model:${vehicleData.model}</p>
+                    <p>Manufacturer:${vehicleData.manufacturer}</p>
+                    <p>Cost_in_credits:${vehicleData.cost_in_credits}</p>
+                    <p>Length:${vehicleData.length}</p>
+                `;
+                // Agrega la informacion del planeta al div movies
+                console.log(vehicleInfo);
+                vehiclesInfo.innerHTML = vehicleInfo;
+            })
+            .catch(error => {
+                console.error('Error fetching  vehicle:', error);
+            });
+
+
+
+
         })
-        .catch(error => {
-            console.error('Error al obtener datos del personaje:', error);
-        });
-}
+
+    }
